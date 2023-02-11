1013ae43855601fedab439d2deb2d8deaf7c5164
justin <justin.rasi@gmx.ch>
2023-02-11 14:27:16 +0100

    deploy

--- portfolio/components/Main.jsx
+++ portfolio/components/Main.jsx
@@ -5,6 +5,7 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
+  //
   return (
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
